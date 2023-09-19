f17697f1a46871a78a5398dbe40feff683a8860d
John Reitano <jreitano@gmail.com>
2023-09-18 22:26:24 -0700

    configure to run with single worker (but multiple threads for handling many requests)

--- puma.rb
+++ puma.rb
@@ -1,4 +1,4 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 1)
+workers Integer(ENV['WEB_CONCURRENCY'] || 0)
 threads_count = Integer(ENV['THREAD_COUNT'] || 40)
 threads threads_count, threads_count
 port        ENV['PORT']     || 3000
