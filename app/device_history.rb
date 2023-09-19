e5d4464e2d64b23efbd155cf7f54dd3f906bf581
John Reitano <jreitano@gmail.com>
2023-09-18 22:08:48 -0700

    store all reading data instead of just summary

--- app/device_history.rb
+++ app/device_history.rb
@@ -12,7 +12,7 @@ class DeviceHistory
   def add_reading(time, count)
     @lock.with_write_lock do
       return if @readings.has_key?(time)
-      @readings[time] = true
+      @readings[time] = { time: time, count: count }
       @latest_time = time if @latest_time.nil? || time > @latest_time
       @cumulative_count += count
     end
